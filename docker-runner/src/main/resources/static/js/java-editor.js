2742425b51f34cbb9d2d36e9e8b7f1dea42f076c
hiphop5782 <zdlsz@hanmail.net>
2022-09-28 16:17:55 +0900

    화면 및 플러그인 수정중

--- docker-runner/src/main/resources/static/js/java-editor.js
+++ docker-runner/src/main/resources/static/js/java-editor.js
@@ -19,19 +19,28 @@
 
 	$.fn.javaEditor = function(options){
 		
-		const settings = merge({}, options || {});
+		const settings = merge({
+			code:'',
+			execute:true,
+			server:"/docker-runner/docker/run/java/",
+			version:11,
+		}, options || {});
 		
 		return this.each(function(){
 			//add class
 			$(this).addClass("java-editor");
+			
+			//add edit-panel
+			const editPanel = $("<div>").addClass("edit-panel");
+			$(this).append(editPanel);
 
 			//add editor
 			const editor = $("<textarea>").addClass("editor").attr("spellcheck", "false");
-			$(this).append(editor);
+			editPanel.append(editor);
 			
 			//add viewer
 			const viewer = $("<div>").addClass("viewer").addClass("language-java");
-			$(this).append(viewer);
+			editPanel.append(viewer);
 			
 			let backupKey = null;
 			editor.keydown(function(e){
@@ -86,8 +95,56 @@
 				viewer.scrollLeft($(this).scrollLeft());
 			});
 			
-			//iframe
-			const iframe = $("<iframe>").addClass("terminal");
+			//set code
+			editor.val(settings.code);
+			viewer.text(settings.code);
+			hljs.highlightElement(viewer.get(0));
+			
+			
+			if(settings.execute){
+				//result panel
+				const executePanel = $("<div>").addClass("result-panel");
+				$(this).append(executePanel);
+				
+				//button panel
+				const buttonPanel = $("<div>").addClass("button-panel");
+				executePanel.append(buttonPanel);
+				
+				//execute button
+				const button = $("<button>").addClass("execute-btn").text("Execute");
+				buttonPanel.append(button);
+				
+				//terminal
+				const terminal = $("<iframe>").addClass("terminal");
+				executePanel.append(terminal);
+
+				//button event				
+				button.click(function(){
+					const value = editor.val();
+					if(!value){
+						return;
+					}
+					
+					$.ajax({
+						url:settings.server+settings.version,
+						type:"post",
+						data:JSON.stringify({code:value}),
+						contentType:"application/json",
+						dataType:"text",
+						beforeSend:function(){
+							button.prop("disabled", true);
+						},
+						success:function(resp){
+							terminal.addClass("active");
+							terminal.attr("src", resp);
+						},
+						complete:function(){
+							button.prop("disabled", false);
+						},
+					});
+					
+				});
+			}
 		});
 	}
 })(jQuery);
--- docker-runner/src/main/webapp/WEB-INF/views/home.jsp
+++ docker-runner/src/main/webapp/WEB-INF/views/home.jsp
@@ -14,44 +14,24 @@
     <script src="${pageContext.request.contextPath}/js/java-editor.js"></script>
     <script>
         $(function(){
-            $("button").click(function(){
-                var code = $("textarea").val();
-                if(!code) return;
-                
-                $.ajax({
-                    url:"${pageContext.request.contextPath}/docker/run/java/11",
-                    type:"post",
-                    data:JSON.stringify({code:code}),
-                    contentType:"application/json",
-                    dataType:"text",
-                    success:function(resp){
-                        $("iframe").attr("src", resp);
-                    },
-                });
-            });
-            
-        	$(".code-runner").javaEditor();
+        	let code = "";
+        	code += "import java.lang.*;\r\n";
+        	code += "public class Hello {\r\n";
+        	code += "\tpublic static void main(String[] args){\r\n";
+        	code += "\t\t//insert code\r\n";
+        	code += "\t\tfor(int i=1; i<=5; i++){\r\n";
+        	code += "\t\t\tSystem.out.println(\"Hello World \" + i);\r\n";
+        	code += "\t\t}\r\n";
+        	code += "\t}\r\n";
+        	code += "}\r\n";
+        	
+        	$(".code-runner").javaEditor({
+        		code:code,
+        	});
         });
     </script>
 </head>
 <body>
-    <textarea rows="15" style="width:100%" class="editor">import java.util.*;
-public class Hello {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("입력 : ");
-        int value = sc.nextInt();
-        sc.close();
-
-        System.out.println("입력된 값 : " + value);
-    }
-}</textarea>
-    <button>실행</button>
-    <hr>
-    <iframe width="100%" height="200"></iframe>
-    
-    <hr>
-    
     <div class="code-runner"></div>
 </body>
 </html>
